2e25ef3f1a4124dccc800744e0b84f3db40dd70e
AvniSolanki02 <avnisolanki02@gmail.com>
2024-05-18 14:40:24 +0530

    Upload Successfully

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulnOn(){
+    let bulbobj = document.querySelector(".bulbOff");
+    bulbobj.setAttribute("src","image.jpg");
+}
+
+function bulbOff(){
+    let bulbobj = document.querySelector(".bulbOff");
+    bulbobj.setAttribute("src","bulb.jpg");
+}
